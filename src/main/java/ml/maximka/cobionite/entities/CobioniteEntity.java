b9a21b8d80cc0e2889f453e59724cada2c0c4eb7
Maxim Levanov <maximka14@hotmail.co.il>
2020-07-29 14:39:03 +0300

    Added custom damage and duration for cobionite.

--- src/main/java/ml/maximka/cobionite/entities/CobioniteEntity.java
+++ src/main/java/ml/maximka/cobionite/entities/CobioniteEntity.java
@@ -13,20 +13,24 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class CobioniteEntity extends SnowballEntity {
+    private final float damage;
     private final int fireChance;
+    private final int fireDuration;
 
-    public CobioniteEntity(World world, LivingEntity thrower, int fireChance) {
+    public CobioniteEntity(World world, LivingEntity thrower, float damage, int fireChance, int fireDuration) {
         super(world, thrower);
+        this.damage = damage;
         this.fireChance = fireChance;
+        this.fireDuration = fireDuration;
     }
 
     protected void onImpact(RayTraceResult result) {
         if (result.getType() == RayTraceResult.Type.ENTITY) {
             Entity entity = ((EntityRayTraceResult) result).getEntity();
             if (this.shouldDamage(entity)) {
-                entity.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 4);
+                entity.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), this.damage);
                 if (this.rand.nextInt() % this.fireChance == 0) {
-                    entity.setFire(2);
+                    entity.setFire(this.fireDuration);
                 }
             }
         }
